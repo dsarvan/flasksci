4c938d9f0ed105f9a0f73e6d4e5ec7d9b11b8abd
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-09-07 19:32:10 +0530

    replace NumberRange validator with check_interval function

--- model.py
+++ model.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python
 
+import functools
 from wtforms import Form, FloatField, validators
 from math import pi
+
+def check_interval(form, field, min_value=None, max_value=None):
+	"""Form validation: failure if value is outside an interval"""
+	if min_value is not None:
+		failure = True if field.data < min_value else False
+	if max_value is not None:
+		failure = True if field.data > max_value else False
+
+	if failure:
+		raise validators.ValidationError(
+			f"{field.name} = {field.data} not in [{'-infty' if min_value is None else min_value}, {'infty' if max_value is None else max_value}]")
+
+def interval(min_value=None, max_value=None):
+	return functools.partial(check_interval, min_value=min_value, max_value=max_value)
 
 def check_T(form, field):
 	"""Form validation: failure if T > 30 periods"""
@@ -17,16 +32,16 @@ def check_T(form, field):
 class InputForm(Form):
 	A = FloatField(
 		label='amplitude (m)', default=1.0,
-		validators=[validators.InputRequired(), validators.NumberRange(0, 1E+20)])
+		validators=[validators.InputRequired(), interval(0, None)])
 
 	b = FloatField(
 		label='damping factor (kg/s)', default=0,
-		validators=[validators.InputRequired(), validators.NumberRange(0, 1E+20)])
+		validators=[validators.InputRequired(), interval(0, None)])
 
 	w = FloatField(
 		label='frequency (1/s)', default=2*pi,
 		validators=[validators.InputRequired()])
 
 	T = FloatField(
-		label='time interval (s)', default=6*pi,
+		label='time interval (s)', default=20,
 		validators=[validators.InputRequired(), check_T])
