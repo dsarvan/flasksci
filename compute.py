466585c629f145bacae5cafb15a303945b60a10d
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-09-08 19:32:10 +0530

    embed png plot in the html output

--- compute.py
+++ compute.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 
+import base64
+from io import BytesIO
 from numpy import exp, cos, linspace
-import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 import os, time, glob
-import matplotlib
-matplotlib.use('SVG')
+import matplotlib.style
+import matplotlib as mpl
+mpl.use('Agg')
 
-plt.style.use("classic")
-plt.rc("text", usetex="True")
-plt.rc("figure", titlesize=12)
-plt.rc("pgf", texsystem="pdflatex")
-plt.rc("axes", labelsize=14, titlesize=14)
-plt.rc("font", family="serif", weight="normal", size=12)
+mpl.style.use("classic")
+mpl.rc("text", usetex="True")
+mpl.rc("figure", titlesize=12)
+mpl.rc("pgf", texsystem="pdflatex")
+mpl.rc("axes", labelsize=14, titlesize=14)
+mpl.rc("font", family="serif", weight="normal", size=12)
 
 
 def damped_vibrations(t, A, b, w):
@@ -21,22 +24,17 @@ def compute(A, b, w, T, resolution=500):
 	"""return filename of plot of the damped_vibrations function"""
 	t = linspace(0, T, resolution+1)
 	u = damped_vibrations(t, A, b, w)
-	fig, ax = plt.subplots() # needed to avoid adding curves in plot
+	fig = Figure() # needed to avoid adding curves in plot
+	ax = fig.subplots()
 	ax.plot(t, u, "r", lw=1)
 	ax.grid(True, which="both")
+	ax.set(xlim=(0, T), ylim=(-A, A))
 	ax.set(xlabel="$t$", ylabel="$u(t)$")
 	ax.set_title(f"$A = {A}\,m, b = {b}\,kg{{\cdot}}s^{{-1}}, w = {w:.2f}\,s^{{-1}}$")
-	if not os.path.isdir('static'):
-		os.mkdir('static')
-	else:
-		# remove old plot files
-		for filename in glob.glob(os.path.join('static', '*.svg')):
-			os.remove(filename)
 
-	# use time in filename that the browser has not chached
-	plotfile = os.path.join('static', str(time.time()) + '.svg')
-	plt.savefig(plotfile)
-	return plotfile
-
-if __name__ == '__main__':
-	print(compute(1, 0.1, 1, 20))
+	# make Matplotlib write to BytesIO file object
+	# and grab return the object's string
+	figfile = BytesIO() # save it to a temporary buffer file
+	fig.savefig(figfile, format='png')
+	figdata_png = base64.b64encode(figfile.getbuffer()).decode("ascii") # embed plot
+	return figdata_png
