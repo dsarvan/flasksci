dc0fb085ee65ce93812aa5b1ce7027d3fc54eaa3
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-09-14 19:32:10 +0530

    command-line argument svg indicate SVG instead of PNG plot

--- controller.py
+++ controller.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python
 
+import sys
 from flask import Flask, render_template, request
-from compute import compute
 from model import InputForm
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
-def index():
-	form = InputForm(request.form)
-	if request.method == 'POST' and form.validate():
-		result = compute(form.A.data, form.b.data, form.w.data, form.T.data)
-	else:
-		result = None
 
-	return render_template("view.html", form=form, result=result)
+@app.route("/", methods=["GET", "POST"])
+def index():
+    form = InputForm(request.form)
+    if request.method == "POST" and form.validate():
+        result = compute(form.A.data, form.b.data, form.w.data, form.T.data)
+    else:
+        result = None
+
+    return render_template(template, form=form, result=result)
+
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    svg = True if sys.argv == "svg" else False
+
+    if svg:
+        from compute import compute_svg as compute
+        template = "view_svg.html"
+
+    else:
+        from compute import compute_png as compute
+        template = "view_png.html"
+
+    app.run(debug=True)
